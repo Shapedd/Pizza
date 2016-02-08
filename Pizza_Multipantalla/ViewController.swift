523e24a09f15148cdb86df39473c764551cbf48e
Carlos Sanchez San Segundo <carlos.sanchez-sansegundo@prosegur.com>
2016-02-08 22:26:20 +0100

    Initial Commit

---
+++ Pizza_Multipantalla/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Pizza_Multipantalla
+//
+//  Created by Carlos Sanchez San Segundo on 8/2/16.
+//  Copyright © 2016 Carlos Sanchez San Segundo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
